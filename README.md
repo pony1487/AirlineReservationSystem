f8ae438eec587eadbb4d111d53c2b8931300ba95
connrona <connrona@amazon.com>
2024-02-13 16:48:31 +0000

    Add basic Kafka for counting successful or failed bookings

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 ### Using API
 ```
-curl -v --user <someUser>:<somePassword> http://localhost:9090/booking  --request POST --header "Content-Type: application/json" --data '{"flightId":"3", "customerId":"2"}'
+# Making booking
+curl -v --user <someUser>:<somePassword> http://localhost:9090/booking  --request POST --header "Content-Type: application/json" --data '{"flightId":"1", "customerId":"1"}'
+```
+
+```
+# Reset full flight
+curl -v --user apiUser:12345 http://localhost:9090/plane/resetReservedSeats/{flight_number}  --request POST
+```
+
+### Running Kafka
+```
+# Go to Kafka dir
+cd /path/to/kafka_2.13-3.6.1
+
+# Start the ZooKeeper service
+bin/zookeeper-server-start.sh config/zookeeper.properties
+
+# Open a new terminal and run
+bin/kafka-server-start.sh config/server.properties
 ```
--- src/main/java/com/example/airlinereservationsystem/services/BookingService.java
+++ src/main/java/com/example/airlinereservationsystem/services/BookingService.java
@@ -1,5 +1,6 @@
 package com.example.airlinereservationsystem.services;
 
+import com.example.airlinereservationsystem.Reporting.BookingMessageProducer;
 import com.example.airlinereservationsystem.model.Booking;
 import com.example.airlinereservationsystem.model.Customer;
 import com.example.airlinereservationsystem.model.Flight;
@@ -8,6 +9,7 @@ import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -27,6 +29,17 @@ public class BookingService {
     @Autowired
     private PlaneService planeService;
 
+    @Autowired
+    private BookingMessageProducer bookingMessageProducer;
+
+    @Autowired
+    @Qualifier("topicName")
+    private String topicName;
+
+    // TODO move to shared class as this is used in BookingMessageConsumer
+    private final String errorMessageHeader = "[FAILED]";
+    private final String successMessageHeader = "[SUCCESS]";
+
     Logger logger = LoggerFactory.getLogger(BookingService.class);
 
     public Booking addBooking(Booking booking) {
@@ -36,7 +49,9 @@ public class BookingService {
         Optional<Customer> customerOptional = customerService.getCustomerById(customerId);
         if (customerOptional.isEmpty()) {
             String errorMessage = "Customer: " + customerId + " is not registered.";
+            String kafkaMessage = errorMessageHeader + errorMessage;
             logger.error(errorMessage);
+            bookingMessageProducer.sendMessage(topicName, kafkaMessage);
             // TODO better handling.
             throw new IllegalStateException(errorMessage);
         }
@@ -44,7 +59,9 @@ public class BookingService {
         Optional<Flight> flightOptional = flightService.getFlightById(flightId);
         if (flightOptional.isEmpty()) {
             String errorMessage = "Flight: " + flightId + " does not exist.";
+            String kafkaMessage = errorMessageHeader + errorMessage;
             logger.error(errorMessage);
+            bookingMessageProducer.sendMessage(topicName, kafkaMessage);
             // TODO better handling.
             throw new IllegalStateException(errorMessage);
         }
@@ -53,7 +70,9 @@ public class BookingService {
         int planeId = flight.getPlaneId();
         if (!planeService.planeHasRoom(planeId)) {
             String errorMessage = "Plane: " + flightId + " is full.";
+            String kafkaMessage = errorMessageHeader + errorMessage;
             logger.error(errorMessage);
+            bookingMessageProducer.sendMessage(topicName, kafkaMessage);
             // TODO better handling.
             throw new IllegalStateException(errorMessage);
         }
@@ -62,12 +81,17 @@ public class BookingService {
         if (bookingOptional.isPresent()) {
             Booking existingBooking = bookingOptional.get();
             String errorMessage = "Customer: " + customerId + " is already booked on Flight: " + flightId + " with Booking: " + existingBooking.getBookingId();
+            String kafkaMessage = errorMessageHeader + errorMessage;
             logger.error(errorMessage);
+            bookingMessageProducer.sendMessage(topicName, kafkaMessage);
             // TODO better handling.
             throw new IllegalStateException(errorMessage);
         }
 
-        return makeBooking(booking, planeId);
+        Booking completedBooking = makeBooking(booking, planeId);
+        String kafkaMessage = successMessageHeader + " booking " + completedBooking.getBookingId() + " was successful";
+        bookingMessageProducer.sendMessage(topicName, kafkaMessage);
+        return completedBooking;
     }
 
     @Transactional
